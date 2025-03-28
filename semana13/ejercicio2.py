4665c440347ffa069d07dfabca1fe0d2c2f378e8
Randall-Aguilar-eng <you@example.com>
2025-03-28 17:44:57 -0600

    semana13 correccion 3

--- semana13/ejercicio2.py
+++ semana13/ejercicio2.py
@@ -5,21 +5,24 @@ y arroje una excepción de no ser así.
 """
 
 def data_type_decorator(func):
-    def wrapper(*args):
+    def wrapper(*kwargs):
         print("Printing items...")
-        for arg in args:
-            if isinstance(arg, int):
-                print((f"\nValidating this item: {arg}."))
-                print((f"This item {arg} is an integer."))
+        for kwarg in kwargs:
+            if isinstance(kwarg, int):
+                print((f"\nValidating items: {kwarg}."))
+                print((f"This item {kwarg} is an integer."))
+            elif isinstance(kwarg, float):
+                print((f"\nValidating items: {kwarg}."))
+                print((f"This item {kwarg} is an float."))
             else:
-                print((f"\nValidating this item: {arg}."))
-                raise TypeError(f"This item {arg} is not an integer.")
-        return func(*args)
+                print((f"\nValidating this item: {kwarg}."))
+                raise TypeError(f"This item {kwarg} is not an integer.")
+        return func(*kwargs)
     return wrapper
 
 
 @data_type_decorator
-def print_data(*args):
-    return args
+def print_data(*kwargs):
+    return kwargs
 
-print_data(20, "hello")
+print_data(20, 23.5, "hello", {"1": 0})
