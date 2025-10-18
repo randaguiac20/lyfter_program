f9e94967159655583775d2f83862d17796196ee8
randaguiac20 <randagui@gmail.com>
2025-10-17 19:16:31 -0600

    work in progress

--- module2/semana6/ejercicio3_transactions.sql
+++ module2/semana6/ejercicio3_transactions.sql
@@ -16,9 +16,5 @@ UPDATE orders
 SET status = 'Returned'
 WHERE order_id = 'O001';
 
--- If there are any critical errors, rollback to savepoint
--- This would typically be handled by application logic
--- SELECT CASE WHEN final_status LIKE 'ERROR%' THEN ROLLBACK TO order_created END;
-
 -- Si todo tuvo éxito, confirmamos los cambios.
 COMMIT;
